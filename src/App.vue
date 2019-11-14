bf9d63b579b97bfd4500431e347c696bec06f7d7
oncusener <oncu.sener@gmail.com>
2019-11-14 16:41:12 +0300

    Vue Js Components examples

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+   <app-header> </app-header>
+    <hr>
+    <div class="row">
+    <app-servers> </app-servers>
+    <app-serverDetails> </app-serverDetails>
+      
+    </div>
+    <hr>
+ <app-footer> </app-footer>
+  </div>
+</template>
+
+<script>
+import Header from "./Header"
+import Footer from "./Footer"
+import Servers from "./Servers"
+import ServerDetails from "./ServerDetails"
+export default{
+  components:{
+    "app-header" : Header,
+    "app-footer" :Footer,
+    "app-servers" :Servers,
+    "app-serverDetails" : ServerDetails,
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
